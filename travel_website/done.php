baf41cea342362c364a07aa9bc29cc3129f73e6b
Lakshay Malhotra <68064745+Lakshay11254@users.noreply.github.com>
2023-02-07 21:35:10 +0530

    Add files via upload

---
+++ travel_website/done.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+ <!-- swiper css link  -->
+ <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+<!-- font awesome cdn link  -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+<!-- custom css file link  -->
+<link rel="stylesheet" href="css/style.css">
+
+
+</head>
+<body>
+    <div class="heading" style="background:url(images/header-bg-3.png) no-repeat">
+       <h1> Thankyou!! Further details will be shared soon.</h1>
+    <button class="btn" style="margin-left=" type="button" onclick="window.location.href = 'book.php';">
+        Go Back
+    </button>
+
+    </div>
+ 
+
+</body>
+</html>
